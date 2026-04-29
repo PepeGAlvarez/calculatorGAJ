e1033fb2e58fef1e5904eb420e667116965dd7d0
PepeGAlvarez <josegalvarez1977@gmail.com>
2026-04-29 07:38:14 +0200

    Corrigiendo errores de carpetas

---
+++ src/main/java/com/dam/calculatorGAJ/CalculatorGAJApplicationTests.java
@@ -0,0 +1,22 @@
+package com.dam.calculatorGAJ;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.beans.factory.annotation.Autowired; // Importación necesaria
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorGAJApplicationTests {
+
+    @Autowired // Esto permite que Spring cargue la clase Calculator automáticamente
+    private Calculator calculator;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(3, 2));
+    }
+}
