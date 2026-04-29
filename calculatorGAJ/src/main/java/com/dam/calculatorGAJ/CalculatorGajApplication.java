634805598d935f585ae374f140a629a3e8007312
PepeGAlvarez <josegalvarez1977@gmail.com>
2026-04-29 05:11:15 +0200

    commit

---
+++ calculatorGAJ/src/main/java/com/dam/calculatorGAJ/CalculatorGajApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorGAJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorGajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorGajApplication.class, args);
+	}
+
+}
