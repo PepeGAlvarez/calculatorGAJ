dbd417ec3218959349809b6d5c808beceb689648
PepeGAlvarez <josegalvarez1977@gmail.com>
2026-04-29 16:46:28 +0200

    Mofificando GAjApplication.java

--- src/main/java/com/dam/calculatorGAJ/CalculatorGajApplication.java
+++ src/main/java/com/dam/calculatorGAJ/CalculatorGajApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalculatorGajApplication {
+public class CalculatorGAJApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorGajApplication.class, args);
+		SpringApplication.run(CalculatorGAJApplication.class, args);
 	}
 
 }
